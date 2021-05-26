d7bf232c8436ae14e5a08bb2d2d20432be2046d2
Tu Trinh <tutrinh@berkeley.edu>
2021-05-26 09:57:17 -0700

    rewrite filter search logic for new UI, add location options

--- scraper.py
+++ scraper.py
@@ -37,6 +37,7 @@ PAGE_DEPTH = CONFIG["PAGE_DEPTH"] # Change this number to determine the scrape p
 USERNAME_AUTH = CONFIG["USERNAME_AUTH"] # Change this to your LinkedIn Account username
 PASSWORD_AUTH = CONFIG["PASSWORD_AUTH"] # Change this to your LinkedIn Account password
 DRIVER_PATH = CONFIG["DRIVER_PATH"] # Change this to your Webdriver Path
+LOCATIONS = CONFIG["LOCATIONS"]
 
 
 def run():
@@ -55,8 +56,7 @@ def run():
             pass
 
     def input_locator(elements, field):
-        elements = [
-            element for element in elements if element.get_attribute("type") == "text"]
+        elements = [element for element in elements if element.get_attribute("type") == "text"]
         for element in elements:
             if field in element.get_attribute("placeholder"):
                 return element
@@ -64,8 +64,19 @@ def run():
     def button_locator(elements, field):
         print("new call")
         for element in elements:
-            print(element.get_attribute("data-control-name"))
-            if field == element.get_attribute("data-control-name"):
+            print(element.get_attribute("aria-label"))
+            if field == element.get_attribute("aria-label"):
+                return element
+
+    def space_locator(elements, field):
+        for element in elements:
+            if field == element.get_attribute("class"):
+                return element
+
+    def filter_locator(elements, field):
+        for element in elements:
+            # print("DAMNDAMN ", element.get_attribute("innerHTML"))
+            if field in element.get_attribute("innerHTML"):
                 return element
 
     def scrape(driver, url, row, company):
@@ -74,7 +85,7 @@ def run():
             if page == 1:
                 page_url = url
             else:
-                page_url = title_url + "&page=" + str(page)
+                page_url = url + "&page=" + str(page)
             # print("Scraping this URL: " + page_url)
             driver.get(page_url)
             time.sleep(2)
@@ -125,14 +136,14 @@ def run():
 
     # BROWSER HEADLESS HEADLESS SETUP
 
-    # chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument("--headless")
     # chrome_options.add_argument("--window-size=1920x1080")
-    # chrome_options.binary_location = '/Applications/Google Chrome'
+    # chrome_options.binary_location = '/Users/tutrinh/Desktop/Google\ Chrome.app'
 
     # browser = webdriver.Chrome(executable_path="chromedriver.exe",   chrome_options=chrome_options)
 
-    browser = webdriver.Chrome(executable_path=DRIVER_PATH)
+    browser = webdriver.Chrome(executable_path = DRIVER_PATH, chrome_options = chrome_options)
 
     while True:
         try:
@@ -162,50 +173,106 @@ def run():
         #     "https://www.linkedin.com/search/results/people/?origin=DISCOVER_FROM_SEARCH_HOME"
         #     )
         # Enter Locations US and San Francisco Bay Area Manually
-        query_string = "facetGeoRegion=%5B%22us%3A0%22%2C%22us%3A84%22%5D&origin=FACETED_SEARCH"
-        browser.get("https://www.linkedin.com/search/results/people/?{}".format(query_string))
+        # query_string = "facetGeoRegion=%5B%22us%3A0%22%2C%22us%3A84%22%5D&origin=FACETED_SEARCH"
+        # browser.get("https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22us%3A0%22%2C%22us%3A84%22%5D&origin=FACETED_SEARCH")
+        browser.get("https://www.linkedin.com/search/results/people/?origin=SWITCH_SEARCH_VERTICAL")
         time.sleep(5)
 
         # ALL FILTERS BUTTON
-        button_elements = browser.find_elements_by_tag_name("button")
-        all_filters_button = button_locator(button_elements, "all_filters")
-        all_filters_button.click()
+        # button_elements = browser.find_elements_by_tag_name("button")
+        # all_filters_button = button_locator(button_elements, "All filters")
+        # all_filters_button.click()
 
         # ELEMENTS
         input_elements = browser.find_elements_by_tag_name("input")
         button_elements = browser.find_elements_by_tag_name("button")
+        div_elements = browser.find_elements_by_tag_name("div")
 
         # FILTER LOCATIONS
-        location_elem = input_locator(input_elements, "country/region")
-        additional_locations = [] # Only add locations not in ["United States", "San Francisco Bay Area"]
-        for location in additional_locations:
-            location_elem.send_keys(location)
+        # location_elem = input_locator(input_elements, "country/region")
+        # additional_locations = [] # Only add locations not in ["United States", "San Francisco Bay Area"]
+        locations_button = button_locator(button_elements, "Locations filter. Clicking this button displays all Locations filter options.")
+        locations_button.click()
+        for location in LOCATIONS:
+            input_elements = browser.find_elements_by_tag_name("input")
+            button_elements = browser.find_elements_by_tag_name("button")
+            add_location_input = input_locator(input_elements, "Add a location")
+            add_location_input.send_keys(location)
             time.sleep(1)
-            location_elem.send_keys(Keys.DOWN, Keys.RETURN)
+            add_location_input.send_keys(Keys.DOWN, Keys.RETURN)
             time.sleep(1)
+        # input_elements = browser.find_elements_by_tag_name("input")
+        # button_elements = browser.find_elements_by_tag_name("button")
+        # submit_locations = button_locator(button_elements, "Apply current filter to show results")
+        # submit_locations.click()
+        empty_space = space_locator(div_elements, "search-reusables__filters-bar-grouping")
+        empty_space.click()
+        time.sleep(5)
 
         # FILTER CURRENT COMPANY
-        company_elem = input_locator(input_elements, "current company")
-        company_elem.send_keys(company)
+        input_elements = browser.find_elements_by_tag_name("input")
+        button_elements = browser.find_elements_by_tag_name("button")
+        div_elements = browser.find_elements_by_tag_name("section")
+        companies_button = button_locator(button_elements, "Current company filter. Clicking this button displays all Current company filter options.")
+        companies_button.click()
+        input_elements = browser.find_elements_by_tag_name("input")
+        button_elements = browser.find_elements_by_tag_name("button")
+        add_company_input = input_locator(input_elements, "Add a company")
+        add_company_input.send_keys(company)
         time.sleep(1)
-        company_elem.send_keys(Keys.DOWN, Keys.RETURN)
-        time.sleep(3)
+        add_company_input.send_keys(Keys.DOWN, Keys.RETURN)
+        time.sleep(1)
+        div_elements = browser.find_elements_by_tag_name("div")
+        empty_space = space_locator(div_elements, "search-reusables__filters-bar-grouping")
+        empty_space.click()
+        time.sleep(5)
 
-        # APPLY FILTERS
-        print(button_elements)
-        apply_button = browser.find_element_by_class_name("search-advanced-facets__button--apply")
-        # apply_button = button_locator(button_elements, "all_filters_apply")
-        apply_button.click()
-
-        # BASE FILTER URL
-        time.sleep(1.5)
-        filter_url = browser.current_url
-        # print("Filter URL is: " + filter_url)
-
-        # URL FOR EACH TITLE
+        # FILTER TITLE
         for title in TITLES:
-            title_url = filter_url + "&title=" + title
-            curr_row = scrape(browser, title_url, curr_row, company)
+            input_elements = browser.find_elements_by_tag_name("input")
+            button_elements = browser.find_elements_by_tag_name("button")
+            div_elements = browser.find_elements_by_tag_name("section")
+            all_filters_button = button_locator(button_elements, "All filters")
+            all_filters_button.click()
+            input_elements = browser.find_elements_by_tag_name("input")
+            button_elements = browser.find_elements_by_tag_name("button")
+            div_elements = browser.find_elements_by_tag_name("section")
+            filter_elements = browser.find_elements_by_tag_name("label")
+            print("SHITSHITSHIT")
+            print(filter_elements)
+            title_label = filter_locator(filter_elements, "<!---->Title<!---->")
+            print("FUCKFUCKFUCK")
+            print(title_label)
+            title_input = title_label.find_elements_by_tag_name("input")[0]
+            title_input.clear()
+            title_input.send_keys(title)
+            time.sleep(1)
+            input_elements = browser.find_elements_by_tag_name("input")
+            button_elements = browser.find_elements_by_tag_name("button")
+            div_elements = browser.find_elements_by_tag_name("section")
+            results_button = button_locator(button_elements, "Apply current filters to show results")
+            results_button.click()
+            time.sleep(5)
+            print("NANINNANINANIANINA")
+            print(browser.current_url)
+            curr_row = scrape(browser, browser.current_url, curr_row, company)
+
+        # # APPLY FILTERS
+        # print(button_elements)
+        # apply_button = browser.find_element_by_class_name("search-advanced-facets__button--apply")
+        # # apply_button = button_locator(button_elements, "all_filters_apply")
+        # apply_button.click()
+
+        # # BASE FILTER URL
+        # time.sleep(1.5)
+        # filter_url = browser.current_url
+        # # print("Filter URL is: " + filter_url)
+
+        # # URL FOR EACH TITLE
+        # for title in TITLES:
+        #     title_url = filter_url + "&title=" + title.replace(" ", "%20")
+        #     print("FUCK " + title_url)
+        #     curr_row = scrape(browser, title_url, curr_row, company)
 
     df = pd.DataFrame(data=df_data)
     df.to_csv('sourcing.csv')
